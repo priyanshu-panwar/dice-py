eccf2d05bc491e597a5b0b4371c330298efa2bb8
Priyanshu Panwar <priyanshu009ch@gmail.com>
2024-11-09 15:44:52 +0530

    udpate test

--- tests/test_dice.py
+++ tests/test_dice.py
@@ -1,10 +1,6 @@
 import unittest
 import asyncio
 from dice_py.dice import Dice
-import logging
-
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
 
 
 class TestDice(unittest.TestCase):
@@ -17,15 +13,13 @@ class TestDice(unittest.TestCase):
         set_response = self.loop.run_until_complete(
             self.dice.set("test_key", "test_value")
         )
-        logger.debug(f"Set response: {set_response}")
         self.assertEqual(set_response, "OK")
 
     def test_get(self):
         # First, set the key to ensure it exists
-        self.loop.run_until_complete(self.dice.set("test_key", "test_value"))
+        self.loop.run_until_complete(self.dice.set("test_key", 10))
         get_response = self.loop.run_until_complete(self.dice.get("test_key"))
-        logger.debug(f"Get response: {get_response}")
-        self.assertEqual(get_response, "test_value")
+        self.assertEqual(get_response, "10")
 
     def tearDown(self):
         # Clean up by deleting the test key
